8be6d43bac09a39fcc0f03f6aee895c669837aa6
Chuiboi <chuiboi890@gmail.com>
2014-10-19 12:03:05 -0700

    Update Project 7.1.cpp

--- Project 7.1.cpp
+++ Project 7.1.cpp
@@ -9,7 +9,7 @@
 // Project: Project No. 7  Part No. 1
 // 
 // Description:
-// This program will read in user provided data into dynamic arrays. It will then
+// This program will read in user provided data into arrays. It will then
 // display the contents of each array.
 //=================================================================================
 #define _CRT_SECURE_NO_WARNINGS
@@ -17,13 +17,14 @@
 #include <ctime>
 #include <algorithm>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 // Declare function prototypes
 template <class TYPE>
 void ReadData(TYPE x[], int n);
 template <class TYPE>
-void DisplayData(TYPE x, int n);
+void DisplayData(TYPE x[], int n);
 //== main =========================================================================
 //
 //=================================================================================
@@ -44,10 +45,10 @@ int main()
 	cout << "Enter 4 names: "; ReadData(s, 4);
 
 	// Display all arrays
-	cout << "This is array a: "; DisplayData(a, 5);
-	cout << "This is array c: "; DisplayData(c, 6);
-	cout << "This is array d: "; DisplayData(d, 4);
-	cout << "This is array s: "; DisplayData(s, 4);
+	cout << "\tThis is array a: "; DisplayData(a, 5);
+	cout << "\tThis is array c: "; DisplayData(c, 6);
+	cout << fixed << setprecision(2) << "\tThis is array d: "; DisplayData(d, 4);
+	cout << "\tThis is array s: "; DisplayData(s, 4);
 
 	system("PAUSE");
 	return 0;
@@ -58,13 +59,11 @@ int main()
 //		x[] -- array of one dimension
 //		n -- integer size of array
 // Output:
-//		Read in data into a dynamic array of size n
+//		Read in data into an array of size n
 //=================================================================================
 template <class TYPE>
 void ReadData(TYPE x[], int n)
 {
-	*x;
-	x = new TYPE[n];
 	for (int i = 0; i < n; i++)
 		cin >> x[i];
 }
@@ -77,9 +76,22 @@ void ReadData(TYPE x[], int n)
 //		Display the one dimensional array
 //=================================================================================
 template <class TYPE>
-void DisplayData(TYPE x, int n)
+void DisplayData(TYPE x[], int n)
 {
 	for (int i = 0; i < n; i++)
 		cout << x[i] << " ";
 	cout << endl;
 }
+/*-------------------------OUTPUT----------------------------------------------
+Time of execution : Sun Oct 19 12 : 00 : 38 2014
+
+Enter 5 integer numbers : 9 3 8 2 7
+Enter 6 characters : C S U F C S
+Enter 4 decimal numbers : 2.3 7.5 8.33 4.1
+Enter 4 names : Tom Bob Mary John
+	This is array a : 9 3 8 2 7
+	This is array c : C S U F C S
+	This is array d : 2.30 7.50 8.33 4.10
+	This is array s : Tom Bob Mary John
+Press any key to continue . . .
+-----------------------------------------------------------------------------*/
