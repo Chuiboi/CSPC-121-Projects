e8a94e62afa5fa69248aa6b0438eb50d4b4fac03
Chuiboi <chuiboi890@gmail.com>
2014-10-21 16:24:10 -0700

    Update Project 7.5.cpp

--- Project 7.5.cpp
+++ Project 7.5.cpp
@@ -9,8 +9,8 @@
 // Project: Project No. 7  Part No. 5
 // 
 // Description:
-// This program will calculate the 43rd Fibonacci number using both the recursive
-// and non-recursive method.
+// This program will calculate the runtime to calculate the 43rd Fibonacci number 
+// using both the recursive and non-recursive method.
 //=================================================================================
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
@@ -30,11 +30,26 @@ int main()
 	time(&p);
 	cout << "Time of execution: " << ctime(&p) << endl;
 
-	// Call the the functions to calculate the 43rd Fibonacci number
-	cout << "The 43rd Fibonacci number using the recursive method is " << RecursiveFibonacci(43) 
-		<< endl;
+	// Declare runtime points
+	int begin, end;
+
+	// Call the the functions to calculate the 43rd Fibonacci number using
+	// recursive method
+	begin = clock();
+	cout << "The 43rd Fibonacci number using the recursive method is " 
+		<< RecursiveFibonacci(43) << endl;
+	end = clock();
+	cout << "Runtime with recursive method: ";
+	cout << (end - begin) / CLK_TCK << " seconds" << endl;
+
+	// Call the the functions to calculate the 43rd Fibonacci number using
+	// nonrecursive method
+	begin = clock();
 	cout << "The 43rd Fibonacci number using the non-recursive method is "
 		<< NonRecursiveFibonacci(43) << endl;
+	end = clock();
+	cout << "Runtime with nonrecursive method: "; 
+	cout << (end - begin) / CLK_TCK << " seconds" << endl;
 
 	system("PAUSE");
 	return 0;
@@ -49,7 +64,7 @@ int main()
 double RecursiveFibonacci(int n)
 {
 	if (n == 1)
-		return 0;
+		return 1;
 	else if (n == 2)
 		return 1;
 	else
@@ -65,9 +80,9 @@ double RecursiveFibonacci(int n)
 //=================================================================================
 double NonRecursiveFibonacci(int n)
 {
-	double num1 = 0, num2 = 1, num3;
+	double num1 = 1, num2 = 1, num3;
 	if (n == 1)
-		return 0;
+		return 1;
 	else if (n == 2)
 		return 1;
 	else
@@ -82,9 +97,11 @@ double NonRecursiveFibonacci(int n)
 	return num3;
 }
 /*-------------------------OUTPUT----------------------------------------------
-Time of execution: Sat Oct 18 11:19:36 2014
+Time of execution: Tue Oct 21 16:21:12 2014
 
-The 43rd Fibonacci number using the recursive method is 2.67914e+008
-The 43rd Fibonacci number using the non-recursive method is 2.67914e+008
+The 43rd Fibonacci number using the recursive method is 4.33494e+008
+Runtime with recursive method: 21 seconds
+The 43rd Fibonacci number using the non-recursive method is 4.33494e+008
+Runtime with nonrecursive method: 0 seconds
 Press any key to continue . . .
 -----------------------------------------------------------------------------*/
