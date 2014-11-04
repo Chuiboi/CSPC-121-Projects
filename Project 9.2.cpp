c7861d10839fe096b94e1b179447cdfc77fafab1
Chuiboi <chuiboi890@gmail.com>
2014-11-04 15:32:32 -0800

    Update Project 9.2.cpp

--- Project 9.2.cpp
+++ Project 9.2.cpp
@@ -23,12 +23,12 @@ using namespace std;
 class SIX
 {
 private: struct PERSON
-		{
-			string name;
-			int age;
-		};
+{
+			 string name;
+			 int age;
+};
 		 PERSON p[4];
-public: 
+public:
 	void ReadData() // Read data into array p
 	{
 		for (int i = 0; i < 4; i++)
@@ -72,6 +72,7 @@ public:
 		}
 		cout << endl;
 	}
+	~SIX(){ system("PAUSE"); }
 };
 //== main =========================================================================
 //=================================================================================
@@ -90,24 +91,20 @@ int main()
 	a.Display();
 	a.DisplayAboveAve(a.AgeAve());
 
-	system("PAUSE");
 	return 0;
 }
 /*-------------------------OUTPUT-----------------------------------------------------
 Time of execution: Mon Nov 03 09:15:38 2014
-
 Enter a name and age: John 22
 Enter a name and age: Mary 19
 Enter a name and age: Cynthia 33
 Enter a name and age: George 25
-
 Name           Age
 ────────────────────
 John           22
 Mary           19
 Cynthia        33
 George         25
-
 The following people's age is above the average: Cynthia George
 Press any key to continue . . .
 ------------------------------------------------------------------------------------*/
